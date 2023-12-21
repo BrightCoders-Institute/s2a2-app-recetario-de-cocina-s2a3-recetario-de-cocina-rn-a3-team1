0d77fbd088a233dd2172dc398ab0190e8e936e70
Oliver Visoso <ovisosodev@gmail.com>
2023-12-20 19:52:59 -0600

    add new components
    Co-authored-by: CalixtoSD1117 <ccalixtoengelberger117@gmail.com>
    Co-authored-by: Jherrera11 <jherrera11@ucol.mx>
    Co-authored-by: YahirCl <ycortes1@ucol.mx>

--- Recetario/src/components/index.ts
+++ Recetario/src/components/index.ts
@@ -1,5 +1,11 @@
 import ContainerComponent from './ContainerComponent';
+import DishesCardComponent from './DishesCardComponent';
+import FabButtonComponent from './FabButtonComponent';
+import TextComponent from './TextComponent';
 
 export {
   ContainerComponent,
+  DishesCardComponent,
+  FabButtonComponent,
+  TextComponent,
 }
